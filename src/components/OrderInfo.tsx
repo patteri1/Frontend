933503737729b2215d20ca90789a5d74a553efb1
Oliver Roman <oltsu5@hotmail.com>
2024-02-12 14:11:17 +0200

    GraphQL queries added to orders

--- src/components/OrderInfo.tsx
+++ src/components/OrderInfo.tsx
@@ -1,48 +1,60 @@
 import { useState } from 'react'
 import { Modal, Box, Button } from '@mui/material'
-import { Order, OrderRow } from '../TypeDefs'
+import { useQuery, gql } from '@apollo/client'
+import { Order } from './OrderList'
 
-const OrderInfo = () => {
+const GET_ORDER_BY_ID = gql`
+    query Orders($orderId: Int!) {
+        order(id: $orderId) {
+            orderId
+            datetime
+            status
+            location {
+                name
+            }
+            orderRows {
+                amount
+                palletType {
+                    product
+                }
+            }
+        }
+    }
+`
+
+export interface OrderRow {
+    orderId: number
+    palletType: PalletType
+    amount: number
+}
+
+export interface PalletType {
+    palletTypeId: number
+    product: string
+    amount: number
+}
+
+
+const OrderInfo = (props: number) => {
+    const { loading, error, data } = useQuery(GET_ORDER_BY_ID, { variables: {orderId} })
+    console.log(data)
+
     const [open, setOpen] = useState<boolean>(false)
-
+    
     const showModal = () => setOpen(true)
     const hideModal = () => setOpen(false)
-
-    // hardcoded for now: the data for order and orderRow should
-    // be passed here via the order list with the specific orderId
-    const order: Order = {
-        orderId: 1,
-        locationId: 1,
-        locationName: 'Kuljetusliike 1',
-        datetime: '01.02.2024 11:58',
-        status: 'Avattu',
-    }
-    const rows: OrderRow[] = [
-        {
-            orderId: 1,
-            product: 'Tynnyrilava',
-            amount: 10,
-        },
-        {
-            orderId: 1,
-            product: 'Lithiumakkulava',
-            amount: 5,
-        },
-        {
-            orderId: 1,
-            product: 'Pahvilaatikkolava',
-            amount: 20,
-        },
-    ]
+    
+    if (loading) return <p>Loading...</p>
+    if (error) return (<p>Error : { error.message }</p>)
 
     // renders order's pallet types & amounts
-    const palletRows = () => {
-        return rows.map((item, index) => (
-            <p key={index}>
-                {item.product} x{item.amount}
-            </p>
-        ))
-    }
+    // const palletRows = () => {
+    //     return rows.map((item, index) => (
+    //         <p key={index}>
+    //             {item.product} x{item.amount}
+    //         </p>
+    //     ))
+    // }
 
     return (
         <div>
@@ -50,13 +62,12 @@ const OrderInfo = () => {
             <Modal open={open} onClose={hideModal}>
                 <Box sx={styles.box}>
                     <h4>Tilaaja</h4>
-                    <p>{order.locationName}</p>
+                    <p>{data.order.location.name}</p>
                     <h4>Tilaustunnus</h4>
-                    <p>{order.orderId}</p>
+                    <p>{data.order.orderId}</p>
                     <h4>Lavat</h4>
-                    {palletRows()}
                     <h4>Tilattu</h4>
-                    <p>{order.datetime}</p>
+                    <p>{data.order.datetime}</p>
                     <Button
                         style={{ marginTop: '10px', float: 'right' }}
                         onClick={hideModal}
--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,60 +1,89 @@
-import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import OrderInfo from "./OrderInfo";
+import * as React from 'react'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import OrderInfo from './OrderInfo'
+import { useQuery, gql } from '@apollo/client'
 
-interface Row {
-  name: string;
-  status: string;
-  date: string;
+export interface Order {
+    orderId: number
+    datetime: string
+    status: string
+    location: Location
 }
 
-function createData(name: string, status: string, date: string): Row {
-  return { name, status, date };
+interface Location {
+  id: string
+  name: string
+  address: string
+  postCode: string
+  price: number
 }
 
-const rows: Row[] = [
-  createData("Kuljetusliike 1", "Avattu", "1.12.2023"),
-  createData("Kuljetusliike 2", "Avattu", "2.12.2023"),
-  createData("Kuljetusliike 3", "Valmis", "3.12.2023"),
-  createData("Kuljetusliike 4", "Valmis", "4.12.2023"),
-];
+const GET_ORDERS = gql`
+    query AllOrders {
+        allOrders {
+            orderId
+            datetime
+            status
+            location {
+                name
+            }
+        }
+    }
+`
 
 export default function DenseTable() {
+    const { loading, error, data } = useQuery(GET_ORDERS)
+    console.log(data)
 
-  return (
-    <div>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 325 }} size="small" aria-label="a dense table">
-          <TableHead>
-            <TableRow>
-              <TableCell> Tilaaja </TableCell>
-              <TableCell align="right"> Status </TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                style={{ cursor: "pointer" }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.status}</TableCell>
-                <TableCell><OrderInfo/></TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  );
+    if (loading) return <p>Loading...</p>
+    if (error) return (<p>Error : { error.message }</p>)
+
+    return (
+        <div>
+            <TableContainer component={Paper}>
+                <Table
+                    sx={{ minWidth: 325 }}
+                    size="small"
+                    aria-label="a dense table"
+                >
+                    <TableHead>
+                        <TableRow>
+                            <TableCell> Tilaaja </TableCell>
+                            <TableCell align="right"> Status </TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.allOrders.map((row:Order) => (
+                            <TableRow
+                                key={row.orderId}
+                                sx={{
+                                    '&:last-child td, &:last-child th': {
+                                        border: 0,
+                                    },
+                                }}
+                                style={{ cursor: 'pointer' }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.location.name}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {row.status}
+                                </TableCell>
+                                <TableCell>
+                                    <OrderInfo id={row.orderId} />
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    )
 }
