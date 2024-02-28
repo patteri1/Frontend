1f3a4ed9e297e4e31084e7ccb0d880f131f2ec0c
Oliver Roman <oltsu5@hotmail.com>
2024-02-28 11:23:49 +0200

    added form for order

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,11 +1,29 @@
+import { useState } from 'react'
 import OrderList from '../components/OrderList'
 import Page from '../components/Page'
+import OrderForm from '../components/OrderForm'
+import NewOrderButton from '../components/NewOrderButton'
+import Dialog from '@mui/material/Dialog'
 
 const OrderPage = () => {
+    const [isOrderFormOpen, setIsOrderFormOpen] = useState(false)
+
+    const openOrderForm = () => {
+        setIsOrderFormOpen(true)
+    }
+
+    const closeOrderForm = () => {
+        setIsOrderFormOpen(false)
+    }
+
     return (
         <Page>
             <div style={{ paddingTop: 20 }}>
                 <p style={{ fontSize: 20, fontWeight: 'bold' }}>Tilaukset</p>
+                <NewOrderButton onClick={openOrderForm} />
+                <Dialog open={isOrderFormOpen} onClose={closeOrderForm}>
+                    <OrderForm onClose={closeOrderForm} />
+                </Dialog>
                 <OrderList />
             </div>
         </Page>
