10e9449e64ef58d152ceeb9581879bddbce4221f
Topias Mantela <104072848+thoby2@users.noreply.github.com>
2024-02-29 23:29:37 +0200

    Add CustomModal for data display

---
+++ src/components/CustomModal.tsx
@@ -0,0 +1,66 @@
+import { Box, Button, Modal } from '@mui/material'; // Import Box and Button from Material-UI
+
+type Section = {
+    header: string
+    content: JSX.Element | JSX.Element[]
+}
+
+type OptionalButton = {
+    label: string
+    onClick: () => void
+}
+
+type CustomModalProps = {
+    open: boolean
+    hideModal: () => void
+    sections: Section[]
+    optionalButton?: OptionalButton
+}
+
+const CustomModal = ({ open, hideModal, sections, optionalButton }: CustomModalProps) => {
+    return (
+        <Modal open={open} onClose={hideModal}>
+            <Box sx={styles.box}>
+                {sections.map((section, index) => (
+                    <div key={index}>
+                        <h4>{section.header}</h4>
+                        {section.content}
+                    </div>
+                ))}
+                {optionalButton && (
+                    <Button
+                        style={{ marginTop: '10px', float: 'right' }}
+                        onClick={optionalButton.onClick}
+                        variant="contained"
+                    >
+                        {optionalButton.label}
+                    </Button>
+                )}
+                <Button
+                    style={{ marginTop: '10px', float: 'right' }}
+                    onClick={hideModal}
+                    variant="contained"
+                >
+                    Takaisin
+                </Button>
+            </Box>
+        </Modal>
+    );
+};
+
+// move to separate styles component
+const styles = {
+    box: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: '#fff',
+        borderRadius: '25px',
+        lineHeight: '5px',
+        p: 4,
+    },
+}
+
+export default CustomModal
